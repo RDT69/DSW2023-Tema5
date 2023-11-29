20a0046aaf3078fc1757612798b42b6e35f8eb48
Jose Manuel <josediazrdt@gmail.com>
2023-11-29 20:13:49 +0000

    sesion

--- header/validate.php
+++ header/validate.php
@@ -3,7 +3,6 @@ $user = "pepe";
 $password = "1234";
 
 if ($user == $_POST['user']  &&  $password == $_POST['password']) {
- 
   header('Location: login.html');
 } else {
   header('Location: logout.html');
